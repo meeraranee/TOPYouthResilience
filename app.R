350c66bbc86dc013cc58250574e43f8d6447bba7
Meera Patel <meeraranee@gmail.com>
2022-12-01 23:40:40 -0500

    cleaned up online resources

--- app.R
+++ app.R
@@ -16,7 +16,7 @@ mapdf <- read_csv("mapdf1.csv")
 ui <- fluidPage(theme = shinytheme("darkly"),
                 
                 # Application title
-                titlePanel("SunnyR: A PR Youth Mental Health Dashboard",
+                titlePanel("SunnyR: A Mental Health Dashboard for Youth in PR",
                            windowTitle = "Youth Resilience in PR"),
                 
                 # Tab 1
@@ -67,7 +67,8 @@ ui <- fluidPage(theme = shinytheme("darkly"),
 server <- function(input, output) {
   output$youthinfo <- renderUI ({
     HTML(paste(
-      "", "<br><b><img src=https://www.youthdynamics.org/wp-content/uploads/2022/07/Drop-the-Labels-Kids-Shine-When-We-Focus-on-Strengths.png></b></br>", "",
+      "", "<br><b>***IF YOU OR SOMEONE YOU KNOW IS EXPERIENCING AN EMERGENCY, PLEASE DIAL 911 IMMEDIATELY***</br></b>",
+      "<br><b><img src=https://www.youthdynamics.org/wp-content/uploads/2022/07/Drop-the-Labels-Kids-Shine-When-We-Focus-on-Strengths.png></b></br>", "",
       "<br><h3><b>About the App</b></h3>",
       "Youths in Puerto Rico face a range of challenges, such as dealing with Adverse Childhood
       Experiences (ACEs) or negative outcomes of climate disasters. Because of this, some students may struggle with 
@@ -76,10 +77,9 @@ server <- function(input, output) {
       students ages 12-17 face adversity and promote resilience by analyzing potential behavioral
       disorders and finding helpful resources. Mindful activities may also be encouraged in the classroom
       to promote a healthier learning environment and a safe space. If appropriate, educators may also
-      provide information discovered with this app to parents/guardians of targeted youth. (Separate into
-      separate paragraph and go into detail -- no tracking/saving on app. if you come back to app,
-            no history is maintained, will have to re-enter info if coming back to app) No input is being
-            recorded or shared with any other source.</br>",
+      provide information discovered with this app to parents/guardians of targeted youth. *No data will be tracked and
+      saved on this app. If the user leaves the app and comes back to the dashboard, the user will have to re-enter information.
+      No input is being recorded or shared with any other source.</br>",
       
       "<br><h3><b>How To Navigate the Tabs</b></h3>",
       "Educators in Puerto Rico can use this app to find helpful information for their students by using the following tabs:</br>",
@@ -105,7 +105,38 @@ server <- function(input, output) {
     paste("<h3><b>In-Person Resource Locations in PR</b></h3>")
   })
   output$pronline <- renderText({
-    paste("<h3><b>Online Resources</b></h3>")
+    paste("<h3><b>Online Resources</b></h3>", "",
+          "<h4><b>ADHD</b></h4>",
+          "✦ <a href='https://www.understood.org/'>A Day in the Life of a Kid with ADHD</a></b></br>",
+          "✦ <a href='https://www.cdc.gov/ncbddd/adhd/facts.html'>Facts about ADHD</a></b></br>",
+          "✦ <a href='http://www.fffbi.com/info/academy.html'>Fin, Fur, and Feather Bureau of Investigation</a></b></br>",
+          "✦ <a href='https://www.kidshealth.org/en/kids/adhdkid.html'>KidsHealth Organization: ADHD</a></b></br>", "",
+          
+          "<br><h4><b>Anxiety</b></h4>",
+          "✦ <a href='https://www.brave-online.com/'>Brave</a></b></br>",
+          "✦ <a href='https://www.calm.com/'>Calm</a></b></br>",
+          "✦ <a href='https://www.copingcatparents.com/Child_Anxiety_Tales'>Coping Cat Parents</a></b></br>",
+          "✦ <a href='http://www.worrywisekids.org/'>Worry Wise Kids</a></b></br>", "",
+          
+          "<br><h4><b>Depression</b></h4>",
+          "✦ <a href='https://www.erikaslighthouse.org/'>Erika's Lighthouse</a></b></br>",
+          "✦ <a href='https://www.heardalliance.org/'>Health Alliance</a></b></br>",
+          "✦ <a href='https://www.thetrevorproject.org/'>The Trevor Project</a></b></br>",
+          "✦ <a href='https://twloha.com/'>To Write Love On Her Arms</a></b></br>", "",
+          
+          "<br><h4><b>Trauma</b></h4>",
+          "✦ <a href='https://www.4kids.us/4kidsepic'>4KIDS EPIC</a></b></br>",
+          "✦ <a href='https://acestoohigh.com/'>ACEs Too High</a></b></br>",
+          "✦ <a href='https://www.ticti.org/'>Trauma Institute & Child Trauma Institute</a></b></br>",
+          "✦ <a href='https://www.traumaresourceinstitute.com/'>Trauma Resource Institute</a></b></br>", "",
+          
+          "<br><h4><b>Other Mental Health</b></h4>",
+          "✦ <a href='https://goaskalice.columbia.edu/'>Go Ask Alice!</a></b></br>",
+          "✦ <a href='https://mentalhealthliteracy.org/'>Mental Health Literacy</a></b></br>",
+          "✦ <a href='https://www.nami.org/home'>National Alliance on Mental Illness</a></b></br>",
+          "✦ <a href='https://au.reachout.com/'>Reach Out</a></b></br>", "",
+          "</br>"
+          )
   })
   output$prmap <- renderLeaflet({
     leaflet(pr) %>%
